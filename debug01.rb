1e4c664290238b945b0817e852e9a9c1ee0990d5
mad013 <marie.daley0@gmail.com>
2016-03-30 20:36:49 -0400

    Delete duplicates

--- debug01.rb
+++
@@ -1,5 +0,0 @@
-list = ["'yvr' => 'Vancouver'", "'yba' => 'Banff'", "'yyz' => 'Toronto'", "'yxx' => 'Abbotsford'", "'ybw' => 'Calgary'"]
-
-# Why is it returning nil instead of first element of the list above
-# Answer: Changes braces to brackets and added double quotes to turn elements into strings 
-puts list[0]
