11e8cd347ddbc0221a7ac1d7c34c411ffd558ad6
mad013 <marie.daley0@gmail.com>
2016-04-29 15:17:08 +0000

    reviews refactor

--- rotten_mangoes/app/controllers/reviews_controller.rb
+++ rotten_mangoes/app/controllers/reviews_controller.rb
@@ -1,12 +1,12 @@
 class ReviewsController < ApplicationController
   
-  def new
-    @movie = Movie.find(params[:movie_id])
+  before_filter :load_movie #loading parent resource from nested resource controller
+
+  def new  
     @review = @movie.reviews.build #same as: @review = Review.new(movie_id: @movie.id), automatic assignment of the foreign key
   end
 
   def create
-    @movie = Movie.find(params[:movie_id])
     @review = @movie.reviews.build(review_params)
     @review.user_id = @current_user.id
       if @review.save
@@ -17,6 +17,10 @@ class ReviewsController < ApplicationController
   end
 
   protected
+
+  def load_movie
+    @movie = Movie.find(params[:movie_id])
+  end 
 
   def review_params
     params.require(:review).permit(:text, :rating_out_of_ten)
